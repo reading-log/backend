0674ca6294becdd4c25070d20ba97ab0b7dc3022
jeondui <jeondui@gmail.com>
2024-03-03 18:07:04 +0900

    Test: 회원 저장 및 삭제 테스트
    - 회원 save 및 soft delete 테스트

---
+++ src/test/java/com/api/readinglog/domain/entity/MemberTest.java
@@ -0,0 +1,31 @@
+package com.api.readinglog.domain.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.api.readinglog.domain.repository.MemberRepository;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class MemberTest {
+
+    @Autowired
+    private MemberRepository repository;
+
+    @Rollback(value = false)
+    @Test
+    @Transactional
+    public void test() {
+        Member member = Member.of("test@test.com", "test", "test", MemberRole.MEMBER);
+
+        Member saveMember = repository.save(member);
+        repository.delete(saveMember);
+
+        List<Member> members = repository.findAll();
+        System.out.println("size: " + members.size());
+    }
+}
