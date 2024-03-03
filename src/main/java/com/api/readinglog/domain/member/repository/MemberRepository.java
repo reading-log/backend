793eac707cffef471416baf38d68bf540abc140e
Dongmin Kim <ddmkim94@gmail.com>
2024-03-03 19:09:40 +0900

    Feat: 회원 패키지 구조 변경
    - domain > member로 변경

--- src/main/java/com/api/readinglog/domain/member/repository/MemberRepository.java
+++ src/main/java/com/api/readinglog/domain/member/repository/MemberRepository.java
@@ -1,6 +1,6 @@
-package com.api.readinglog.domain.repository;
+package com.api.readinglog.domain.member.repository;
 
-import com.api.readinglog.domain.entity.Member;
+import com.api.readinglog.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
