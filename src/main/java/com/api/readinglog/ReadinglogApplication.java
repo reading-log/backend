f2b7092f0d182341cc412ad56be3f65f4c88b4e9
Dongmin Kim <ddmkim94@gmail.com>
2024-03-03 16:15:26 +0900

    프로젝트 생성

---
+++ src/main/java/com/api/readinglog/ReadinglogApplication.java
@@ -0,0 +1,13 @@
+package com.api.readinglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadinglogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadinglogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/readinglog/ReadinglogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.readinglog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadinglogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
