51dc29435e4cb9b3ec2bdf0c172cdaff4063671c
jeondui <jeondui@gmail.com>
2024-03-12 15:58:32 +0900

    Feat: 로그인 API 구현

---
+++ src/main/java/com/api/readinglog/common/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.api.readinglog.common.security;
+
+import com.api.readinglog.domain.member.entity.Member;
+import com.api.readinglog.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(email)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("해당하는 회원을 찾을 수 없습니다."));
+    }
+
+    private UserDetails createUserDetails(Member member) {
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().name())
+                .build();
+    }
+}
