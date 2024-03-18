5dd3323c838ddd040248c99f7472cbd579a1de75
Dongmin Kim <ddmkim94@gmail.com>
2024-03-18 15:06:47 +0900

    Feat: 책 도메인 설계

---
+++ src/main/java/com/api/readinglog/domain/book/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.api.readinglog.domain.book.repository;
+
+import com.api.readinglog.domain.book.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
