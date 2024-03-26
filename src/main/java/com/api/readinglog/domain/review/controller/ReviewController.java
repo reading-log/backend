632bdf6c3c603bc568acbf9707f3a3c210ce97da
Dongmin Kim <ddmkim94@gmail.com>
2024-03-26 16:22:11 +0900

    Feat: 내가 쓴 서평 목록 조회 기능

--- src/main/java/com/api/readinglog/domain/review/controller/ReviewController.java
+++ src/main/java/com/api/readinglog/domain/review/controller/ReviewController.java
@@ -4,12 +4,18 @@ import com.api.readinglog.common.response.Response;
 import com.api.readinglog.common.security.CustomUserDetail;
 import com.api.readinglog.domain.review.controller.dto.request.ModifyRequest;
 import com.api.readinglog.domain.review.controller.dto.request.WriteRequest;
+import com.api.readinglog.domain.review.controller.dto.response.ReviewResponse;
 import com.api.readinglog.domain.review.service.ReviewService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +29,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReviewController {
 
     private final ReviewService reviewService;
+
+    @GetMapping("/{bookId}/me")
+    public Response<Page<ReviewResponse>> reviews(@AuthenticationPrincipal CustomUserDetail user,
+                                                  @PathVariable Long bookId,
+                                                  @PageableDefault(sort = "createdAt", direction = Direction.DESC) Pageable pageable) {
+
+        Page<ReviewResponse> response = reviewService.reviews(user.getId(), bookId, pageable);
+        return Response.success(HttpStatus.OK, "내가 쓴 서평 목록 조회 성공", response);
+    }
 
     @PostMapping("/{bookId}")
     public Response<Void> write(@AuthenticationPrincipal CustomUserDetail user,
--- src/main/java/com/api/readinglog/domain/review/service/ReviewService.java
+++ src/main/java/com/api/readinglog/domain/review/service/ReviewService.java
@@ -8,9 +8,12 @@ import com.api.readinglog.domain.member.entity.Member;
 import com.api.readinglog.domain.member.service.MemberService;
 import com.api.readinglog.domain.review.controller.dto.request.ModifyRequest;
 import com.api.readinglog.domain.review.controller.dto.request.WriteRequest;
+import com.api.readinglog.domain.review.controller.dto.response.ReviewResponse;
 import com.api.readinglog.domain.review.entity.Review;
 import com.api.readinglog.domain.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +25,21 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final MemberService memberService;
     private final BookService bookService;
+
+    @Transactional(readOnly = true)
+    public Page<ReviewResponse> reviews(Long memberId, Long bookId, Pageable pageable) {
+        Member member = memberService.getMemberById(memberId);
+        Book book = bookService.getBookById(bookId);
+
+        Page<ReviewResponse> reviews = reviewRepository.findAllByMemberAndBook(member, book, pageable)
+                .map(ReviewResponse::fromEntity);
+
+        if (reviews.getContent().isEmpty()) {
+            throw new ReviewException(ErrorCode.NOT_FOUND_REVIEW);
+        }
+
+        return reviews;
+    }
 
     public void write(Long memberId, Long bookId, WriteRequest request) {
         Member member = memberService.getMemberById(memberId);
