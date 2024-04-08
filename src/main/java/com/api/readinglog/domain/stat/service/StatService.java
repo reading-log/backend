50cbcc74e4b8322c1c96417ae765586534d2049c
Dongmin Kim <ddmkim94@gmail.com>
2024-04-08 21:51:14 +0900

    Feat: 통계 기능 수정
    - API 응답에 이번달에 받은 좋아요 개수 추가

--- src/main/java/com/api/readinglog/domain/stat/service/StatService.java
+++ src/main/java/com/api/readinglog/domain/stat/service/StatService.java
@@ -3,6 +3,7 @@ package com.api.readinglog.domain.stat.service;
 import com.api.readinglog.domain.book.dto.BookCalendarResponse;
 import com.api.readinglog.domain.book.dto.BookCategoryResponse;
 import com.api.readinglog.domain.book.repository.BookRepository;
+import com.api.readinglog.domain.likesummary.repository.LikeSummaryRepository;
 import com.api.readinglog.domain.stat.controller.dto.StatResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -15,15 +16,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class StatService {
 
     private final BookRepository bookRepository;
+    private final LikeSummaryRepository likeSummaryRepository;
 
     public StatResponse getStats(Long memberId, int month) {
         Long lastMonthTotalBookCount = bookRepository.getBookTotalCountInMonth(memberId, month - 1);
         Long thisMonthTotalBookCount = bookRepository.getBookTotalCountInMonth(memberId, month);
-        List<BookCategoryResponse> bookCountGroupByCategory = bookRepository.getBookCountGroupByCategory(memberId, month);
+        List<BookCategoryResponse> bookCountGroupByCategory = bookRepository.getBookCountGroupByCategoryInMonth(memberId, month);
         List<BookCalendarResponse> bookCalendarInMonth = bookRepository.getBookCalendarInMonth(memberId, month);
+        long thisMonthLikeTotalCount = likeSummaryRepository.getLikeTotalCountInMonth(memberId, month);
 
-        // TODO: 이번달 받은 좋아요 개수 추가
-        return StatResponse.of(month, lastMonthTotalBookCount, thisMonthTotalBookCount, null, bookCountGroupByCategory, bookCalendarInMonth);
+        return StatResponse.of(month, lastMonthTotalBookCount, thisMonthTotalBookCount, thisMonthLikeTotalCount, bookCountGroupByCategory, bookCalendarInMonth);
     }
 
 }
